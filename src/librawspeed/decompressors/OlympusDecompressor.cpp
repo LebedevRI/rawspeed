f5470e99ee0278a15e6be90f3747a97f595e612b
Roman Lebedev <lebedev.ri@gmail.com>
2024-03-01 19:56:51 +0300

    OlympusDecompressorImpl: "fine-tune" block size

--- src/librawspeed/decompressors/OlympusDecompressor.cpp
+++ src/librawspeed/decompressors/OlympusDecompressor.cpp
@@ -250,7 +250,11 @@ void OlympusDecompressorImpl::decompressRow(BitStreamerMSB& bits,
 
   // It's best to process pixels in blocks, that are large-ish,
   // but small-enough to fully fit into CPU L1d cache.
-  constexpr int blockSize = 1;
+  constexpr int L1d = 32 * 1024; // FIXME: get actual value at run-time.
+  constexpr int blockSize =
+      roundUpDivision(L1d, (1U << 7U) * (2 * sizeof(int16_t)));
+  // Going to `1<<8` instead of `1<<7` results in worse perf,
+  // going to `1<<6` does not improve perf, so it's a middle ground.
 
   const auto numBlocks =
       implicit_cast<int>(roundUpDivision(numGroups, blockSize));
